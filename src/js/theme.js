862cc6b83aa0ee4c24f3a142ca8148a389c256dd
AlexandraMarkova <a.markova@ukr.net>
2021-01-25 16:35:32 +0200

    добавляет изминение темы

--- src/js/theme.js
+++ src/js/theme.js
@@ -7,10 +7,21 @@ const checkboxRef = document.querySelector('.theme-switch__toggle');
 
  checkboxRef.addEventListener('change', onBodyLite);
 
- function onBodyLite(event) { 
-     document.body.classList.toggle('dark-theme');
+function onBodyLite(event) {
+   
+  document.body.classList.toggle(Theme.DARK);
+  
+  if (document.body.classList.contains(Theme.DARK)) {
+    localStorage.setItem('theme', 'dark-theme'); 
+  } else localStorage.removeItem('theme');
  }
 
+const localTheme = localStorage.getItem('theme');
+
+if (localTheme) { 
+   onBodyLite();
+  checkboxRef.checked = true;
+}
 
 
 
